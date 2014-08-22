8a610f3aeb10867b9b7d905d833cb8d40cfdc5cf
Louis Gollut <louis.gollut@gmail.com>
2014-08-22 10:23:07 +0200

    Correct a typo in package.js and add version.json and .gitignore to package

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  summary: 'Add Font awesome to meteor project'
+  summary: 'Add Font awesome to meteor project',
   version: '0.1.0',
-  git: 'https://lgollut@bitbucket.org/lgollut/meteor-font-awesome.git'
+  git: 'https://github.com/lgollut/meteor-font-awesome-less.git'
 });
